68c0d36165b9ce31d9a4576edc12c27038571c7f
Abraham Alonzo Velázquez <ornitorrincco@gmail.com>
2017-02-06 16:37:02 -0800

    Pot Display

---
+++ pyopenGL/example01pot.py
@@ -0,0 +1,17 @@
+#This program display a pot 
+
+from OpenGL.GLUT import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+def draw():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glutWireTeapot(0.5)
+    glFlush()
+    
+glutInit(sys.argv)
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+glutInitWindowSize(350, 350)
+glutInitWindowPosition(100, 100)
+glutCreateWindow("My tetera")
+glutDisplayFunc(draw)
+glutMainLoop()
