2dbbd4145abca04263bde48addbcc8bb92761be9
Abraham Alonzo Velázquez <ornitorrincco@gmail.com>
2017-02-14 17:00:47 -0800

    Create example01.cpp

---
+++ win32_api/example01.cpp
@@ -0,0 +1,16 @@
+// This program show a message box(Ansi) with a legend, is necesarry to use user32.lib (linker)
+
+
+#include<windows.h>
+
+int CALLBACK
+WinMain(HINSTANCE hInstance,
+		HINSTANCE hPrevInstance,
+		LPSTR lpCmdLine,
+ 		int nCmdShow){
+
+	MessageBoxA(0, "This is underated","Final Fart", 
+		MB_OK | MB_ICONINFORMATION);
+
+	return (0);
+}
